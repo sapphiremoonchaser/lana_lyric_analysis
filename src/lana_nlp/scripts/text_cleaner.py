917d2de59f00a26e354c95c2a8c4d69e5628f944
violetdiamonds <violetdiamonds@protonmail.com>
2026-07-29 16:10:45 -0500

    lowercase method

--- src/lana_nlp/scripts/text_cleaner.py
+++ src/lana_nlp/scripts/text_cleaner.py
@@ -7,3 +7,18 @@ class TextCleaner:
     def __init__(self):
         pass
 
+    def lowercase(
+        self,
+        text: str
+    ) -> str:
+        """
+        Convert to lowercase.
+        Args:
+            text: text to convert
+
+        Returns:
+            The lowercase text
+        """
+        return text.lower()
+
+
