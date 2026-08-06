b3c1dacc6b175c56acff48d540db44b9d40934ff
violetdiamonds <violetdiamonds@protonmail.com>
2026-08-06 12:21:59 -0500

    sample df and empty df config file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,49 @@
 import nltk
-
+import pandas as pd
+import pytest
 
 def pytest_configure():
     nltk.download(
         "opinion_lexicon",
         quiet=True
+    )
+
+
+@pytest.fixture
+def sample_df():
+    """
+    Sample dataframe.
+    Returns:
+
+    """
+    return pd.DataFrame(
+        {
+            "song": [
+                "Venice Bitch",
+                "Fuck it I love you",
+                "Wildflower Wildfire",
+            ],
+            "album": [
+                "NFR",
+                "NFR",
+                "Blue Banisters",
+            ],
+            "year": [
+                2019,
+                2019,
+                2021,
+            ],
+            "lyrics": [
+                "fresh out of fucks forever",
+                "veins in neon forever",
+                "star drip iv's",
+            ],
+        }
+    )
+
+
+@pytest.fixture
+def empty_df():
+    return pd.DataFrame(
+        columns=["song", "album", "year", "lyrics"]
     )
