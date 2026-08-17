59f25c250c6cd758ea423ff26d6e2a0e1343268f
violetdiamonds <violetdiamonds@protonmail.com>
2026-08-17 16:22:15 -0500

    add vocabulary data

--- src/lana_nlp/analysis/pipeline.py
+++ src/lana_nlp/analysis/pipeline.py
@@ -4,11 +4,13 @@ the data.
 """
 import pandas as pd
 
+from lana_nlp.analysis.vocabulary import VocabularyAnalyzer
 from lana_nlp.features.lyrics_features import LyricsFeatures
 from lana_nlp.preprocessing.data_loader import LyricsDataLoader
 from lana_nlp.preprocessing.text_cleaner import TextCleaner
 from lana_nlp.analysis.readability import ReadabilityAnalyzer
 from lana_nlp.analysis.sentiment import SentimentAnalyzer
+from lana_nlp.analysis.vocabulary import VocabularyAnalyzer
 
 
 def pipeline(
@@ -42,6 +44,29 @@ def pipeline(
     )
 
     features_analyzer.analyze()
+
+    # Vocabulary features
+    vocabulary_analyzer = VocabularyAnalyzer()
+
+    df["lexical_diversity"] = (
+        df["nlp_cleaned_lyrics"]
+        .apply(vocabulary_analyzer.lexical_diversity)
+    )
+
+    df["vocabulary_size"] = (
+        df["nlp_cleaned_lyrics"]
+        .apply(vocabulary_analyzer.vocabulary_size)
+    )
+
+    df["word_frequency"] = (
+        df["nlp_cleaned_lyrics"]
+        .apply(vocabulary_analyzer.word_frequency())
+    )
+
+    df["unique_word_count"] = (
+        df["nlp_cleaned_lyrics"]
+        .apply(vocabulary_analyzer.unique_word_count())
+    )
 
     # Add readability features
     readability_analyzer =  ReadabilityAnalyzer(
