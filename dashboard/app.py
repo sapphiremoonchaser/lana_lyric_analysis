18ed6252c8ee4c3f69bbebc4bb0deaaafbdae016
violetdiamonds <violetdiamonds@protonmail.com>
2026-08-18 12:23:56 -0500

    show dataframes in streamlit

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,5 +1,20 @@
 import streamlit as st
+import pandas as pd
 
+
+# Header of the dashboard
 st.title("Lana Del Rey Lyric Analysis")
 
-st.write("Welcome to my dashboard!")
+# load song and album level dataframes
+song_df = pd.read_csv("./data/processed/song_level_stats.csv")
+album_df = pd.read_csv("./data/processed/album_level_stats.csv")
+
+# Show song level dataframe
+st.write("Song-Level Data")
+st.dataframe(song_df)
+
+# Show album level dataframe
+st.write("Album-Level Data")
+st.dataframe(album_df)
+
+
