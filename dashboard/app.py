3927cd3cad82734d4496c9debb4850ce9934e8f6
violetdiamonds <violetdiamonds@protonmail.com>
2026-08-18 12:30:30 -0500

    build sidebar

--- dashboard/app.py
+++ dashboard/app.py
@@ -2,19 +2,58 @@ import streamlit as st
 import pandas as pd
 
 
-# Header of the dashboard
-st.title("Lana Del Rey Lyric Analysis")
+st.set_page_config(
+    page_title="Lana Del Rey Lyric Analysis",
+    page_icon="🍒",
+    layout="wide"
+)
 
-# load song and album level dataframes
+# --------------------
+# Load data
+# --------------------
 song_df = pd.read_csv("./data/processed/song_level_stats.csv")
 album_df = pd.read_csv("./data/processed/album_level_stats.csv")
 
-# Show song level dataframe
-st.write("Song-Level Data")
-st.dataframe(song_df)
+# --------------------
+# Sidebar
+# --------------------
 
-# Show album level dataframe
-st.write("Album-Level Data")
-st.dataframe(album_df)
+st.sidebar.title("Lana Del Rey")
+
+page = st.sidebar.radio(
+    "Navigate",
+    [
+        "Overview",
+        "Lyrical Style",
+        "Album Comparison",
+        "Song Explorer"
+    ]
+)
+
+# --------------------
+# Page content
+# --------------------
+
+if page == "Overview":
+    st.title("Lana Del Rey Lyric Analysis")
+
+    st.write(
+        "An exploration of Lana Del Rey's lyrics"
+        "using natural language processing."
+    )
+
+elif page == "Lyrical Style":
+
+    st.title("Lyrical Style Over Time")
+
+elif page == "Album Comparison":
+
+    st.title("Album Comparison")
+
+elif page == "Song Explorer":
+
+    st.title("Song Explorer")
+
+
 
 
